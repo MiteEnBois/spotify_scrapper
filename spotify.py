ee40943e6d8f628288ad4198a150e58a37124fc7
MiteEnBois <mystergreen045@gmail.com>
2021-08-04 15:07:30 +0200

    small update

--- spotify.py
+++ spotify.py
@@ -12,6 +12,7 @@ GENIUS_CLIENT_ID = os.getenv('GENIUS_CLIENT_TOKEN')
 auth_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
+# pip install -r requirements.txt
 
 def load_data(filename):
     try:
